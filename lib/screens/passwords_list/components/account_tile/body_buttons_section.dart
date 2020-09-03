e570c89c08416b031e544e12bdeab901da159e0a
LiLatee <marcin.hradowicz@gmail.com>
2020-09-03 22:03:27 +0200

    edit in progress

--- lib/screens/passwords_list/components/account_tile/body_buttons_section.dart
+++ lib/screens/passwords_list/components/account_tile/body_buttons_section.dart
@@ -1,55 +1,116 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 
+import '../../../../utils/constants.dart' as MyConstants;
+
+typedef ToogleEditMode();
+typedef ToogleShowPassword();
+
 class ButtonsSection extends StatelessWidget {
-  const ButtonsSection({
-    Key key,
-  }) : super(key: key);
+  ToogleEditMode toogleEditModeCallback;
+  ToogleShowPassword toogleShowPasswordCallback;
+  ButtonsSection(
+      {Key key, this.toogleEditModeCallback, this.toogleShowPasswordCallback})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    log("NOWE", name: "LOL");
+
     return Row(
       mainAxisAlignment: MainAxisAlignment.center,
       children: <Widget>[
         ButtonBar(
           children: <Widget>[
-            buildFlatButton(
-                context: context,
-                onPressed: () {},
-                icon: Icon(Icons.remove_red_eye),
-                name: 'Show'),
-            buildFlatButton(
-                context: context,
-                onPressed: () {},
-                icon: Icon(Icons.edit),
-                name: 'Edit'),
-            buildFlatButton(
-                context: context,
+            AccountDataTileButton(
+              onPressed: () {
+                toogleShowPasswordCallback();
+              },
+              icon: Icon(Icons.remove_red_eye),
+              label: 'Show',
+              canBePressed: true,
+            ),
+            AccountDataTileButton(
+              onPressed: () {
+                toogleEditModeCallback();
+              },
+              icon: Icon(Icons.edit),
+              label: 'Edit',
+              canBePressed: true,
+            ),
+            AccountDataTileButton(
                 onPressed: () {},
                 icon: Icon(Icons.delete_forever),
-                name: 'Remove'),
-            buildFlatButton(
-                context: context,
-                onPressed: () {},
-                icon: Icon(Icons.add),
-                name: 'Add field'),
+                label: 'Remove'),
+            AccountDataTileButton(
+                onPressed: () {}, icon: Icon(Icons.add), label: 'Add field'),
           ],
         ),
       ],
     );
   }
+}
 
-  FlatButton buildFlatButton(
-      {BuildContext context, Function onPressed, Icon icon, String name}) {
-    return FlatButton(
-      textColor: Theme.of(context).accentColor,
-      onPressed: onPressed,
-      child: Row(
-        children: <Widget>[
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[icon, Text(name)],
-          ),
-        ],
+class AccountDataTileButton extends StatefulWidget {
+  final Function onPressed;
+  final Icon icon;
+  final String label;
+  final bool canBePressed;
+  const AccountDataTileButton({
+    Key key,
+    @required this.onPressed,
+    @required this.icon,
+    @required this.label,
+    this.canBePressed = false,
+  }) : super(key: key);
+
+  @override
+  _AccountDataTileButtonState createState() => _AccountDataTileButtonState();
+}
+
+class _AccountDataTileButtonState extends State<AccountDataTileButton> {
+  Color textColor;
+  Color backgroundColor;
+  @override
+  Widget build(BuildContext context) {
+    textColor ??= Theme.of(context).accentColor;
+    backgroundColor ??= Theme.of(context).primaryColor;
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius:
+            BorderRadius.circular(MyConstants.defaultCircularBorderRadius),
+        color: backgroundColor,
+      ),
+      child: FlatButton(
+        textColor: textColor,
+        onPressed: () {
+          setState(() {
+            if (widget.canBePressed) {
+              // if (textColor == Theme.of(context).accentColor) {
+              //   textColor = MyConstants.pressedButtonColor;
+              // } else {
+              //   textColor = Theme.of(context).accentColor;
+              // }
+
+              if (backgroundColor == Theme.of(context).primaryColor) {
+                backgroundColor = MyConstants.pressedButtonColor;
+              } else {
+                backgroundColor = Theme.of(context).primaryColor;
+              }
+            }
+
+            widget.onPressed();
+          });
+        },
+        child: Row(
+          children: <Widget>[
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[widget.icon, Text(widget.label)],
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/screens/passwords_list/components/body.dart
+++ lib/screens/passwords_list/components/body.dart
@@ -1,12 +1,15 @@
+import 'dart:developer';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-import '../../../utils/constants.dart' as MyConstants;
 import '../../../models/account_data.dart';
 import '../../../modified_flutter_widgets/expansion_panel.dart' as epn;
+import '../../../utils/constants.dart' as MyConstants;
 import 'account_tile/body_buttons_section.dart';
 import 'account_tile/body_fields_section.dart';
 import 'account_tile/header.dart';
+import 'account_tile/account_data_expanded_part.dart';
 
 class Body extends StatefulWidget {
   final List<AccountData> accounts;
@@ -18,46 +21,83 @@ class Body extends StatefulWidget {
 }
 
 class _BodyState extends State<Body> {
-  String expandedValue;
-
+  // List<bool> isExpandedList;
+  Map<String, bool> isEditableMap;
+  Map<String, bool> showPasswordMap;
   @override
   Widget build(BuildContext context) {
+    // isExpandedList ??= List.filled(widget.accounts.length, false);
+    isEditableMap ??= Map.fromIterables(
+        widget.accounts.map((e) => e.accountName),
+        List.filled(widget.accounts.length, false));
+
+    showPasswordMap ??= Map.fromIterables(
+        widget.accounts.map((e) => e.accountName),
+        List.filled(widget.accounts.length, false));
+
     return ListView(children: <Widget>[
       epn.ExpansionPanelList.radio(
-        initialOpenPanelValue: expandedValue,
+        expansionCallback: (panelIndex, isExpanded) {
+          setState(() {
+            // Expanded/Collapsed tile, so turn off edit mode and hide password everywhere.
+            isEditableMap = Map.fromIterables(
+                widget.accounts.map((e) => e.accountName),
+                List.filled(widget.accounts.length, false));
+
+            showPasswordMap = Map.fromIterables(
+                widget.accounts.map((e) => e.accountName),
+                List.filled(widget.accounts.length, false));
+
+            // bool temp = !isExpandedList[panelIndex];
+            // isExpandedList[panelIndex] = !isExpanded && temp;
+          });
+        },
         expandedHeaderPadding:
             EdgeInsets.only(left: MyConstants.defaultPadding * 3),
-        children: widget.accounts
-            .map<epn.ExpansionPanelRadio>(
-                (accountData) => buildExpansionPanel(accountData: accountData))
-            .toList(),
+        children: widget.accounts.map((accountData) {
+          return buildExpansionPanel(
+              accountData: accountData,
+              isEditable: isEditableMap[accountData.accountName],
+              showPassword: showPasswordMap[accountData.accountName]);
+        }).toList(),
       ),
     ]);
   }
 
   epn.ExpansionPanelRadio buildExpansionPanel(
-      {@required AccountData accountData}) {
+      {@required AccountData accountData,
+      @required bool isEditable,
+      @required bool showPassword}) {
     return epn.ExpansionPanelRadio(
         canTapOnHeader: true,
         value: accountData.accountName,
         headerBuilder: (BuildContext context, bool isExpanded) {
-          if (isExpanded) {
-            expandedValue = accountData.accountName;
-          }
-
           return AccountTileHeader(
             accountData: accountData,
           );
         },
-        body: buildExpandedPart(accountData: accountData));
+        body: AccountDataExpandedPart(
+          accountData: accountData,
+          isEditable: isEditable,
+          toogleEditModeCallback: ({accountName}) {
+            toogleEditMode(accountName: accountName);
+          },
+          showPassword: showPassword,
+          toogleShowPasswordCallback: ({accountName}) {
+            toogleShowPassword(accountName: accountName);
+          },
+        ));
   }
 
-  Widget buildExpandedPart({AccountData accountData}) {
-    return Column(
-      children: <Widget>[
-        FieldsSection(accountData: accountData),
-        ButtonsSection(),
-      ],
-    );
+  void toogleEditMode({String accountName}) {
+    setState(() {
+      isEditableMap[accountName] = !isEditableMap[accountName];
+    });
+  }
+
+  void toogleShowPassword({String accountName}) {
+    setState(() {
+      showPasswordMap[accountName] = !showPasswordMap[accountName];
+    });
   }
 }
